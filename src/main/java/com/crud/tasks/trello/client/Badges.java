2597aa3cfda318db4f36a9d6a910366561de5de3
pawchi <pawchi@o2.pl>
2018-04-16 10:20:57 +0200

    Task Trello 18.3

---
+++ src/main/java/com/crud/tasks/trello/client/Badges.java
@@ -0,0 +1,19 @@
+package com.crud.tasks.trello.client;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Badges {
+    @JsonProperty("votes")
+    private int votes;
+
+    @JsonProperty("attachmentByType")
+    private AttachmentByType attachmentByType;
+}
